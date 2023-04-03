06af574ed7cbfddd6e55d911fb37c01a0b55d523
Matt Enlow <enlow@adobe.com>
2023-04-03 13:42:04 -0600

    redesign aliases

--- lib/style/aliases.ex
+++ lib/style/aliases.ex
@@ -24,75 +24,48 @@ defmodule Styler.Style.Aliases do
   This module is more particular than credo for sorting. Notably, it sorts `alias __MODULE__`, whereas Credo allowed
   that alias intermixed anywhere in a group.
   """
-
   @behaviour Styler.Style
 
-  alias Styler.Zipper
+  def run({{:alias, _, _} = alias, %{l: left, r: right} = meta}) do
+    {right, aliases} = consume_alias_group([alias | right], [])
 
-  def run({{:alias, _, _}, _} = zipper) do
-    {zipper, aliases} =
-      zipper
-      |> Zipper.insert_left(:placeholder)
-      |> consume_alias_group([])
-
-    [first | rest] =
+    [last | rest] =
       aliases
+      |> Enum.flat_map(&expand_alias/1)
       # Credo does case-agnostic sorting, so we have to match that here
       |> Enum.map(&{&1, &1 |> Macro.to_string() |> String.downcase()})
       # a splash of deduping for happiness
       |> Enum.uniq_by(&elem(&1, 1))
-      |> Enum.sort_by(&elem(&1, 1))
+      |> List.keysort(1, :desc)
       |> Enum.map(&(&1 |> elem(0) |> set_newlines(1)))
 
-    zipper =
-      zipper
-      |> Zipper.find(:prev, &(&1 == :placeholder))
-      |> Zipper.replace(first)
-
-    rest
-    |> Enum.reduce(zipper, &(&2 |> Zipper.insert_right(&1) |> Zipper.right()))
-    |> Zipper.update(&set_newlines(&1, 2))
+    {set_newlines(last, 2), %{meta | r: right, l: rest ++ left}}
   end
 
   def run(zipper), do: zipper
 
-  defp set_newlines({node, meta, children}, newline) do
-    meta = Keyword.update(meta, :end_of_expression, [newlines: newline], &Keyword.put(&1, :newlines, newline))
-    {node, meta, children}
-  end
-
-  defp consume_alias_group({{:alias, meta, _} = alias, _} = zipper, aliases) do
-    aliases = expand_alias(alias, aliases)
-    zipper = Zipper.remove(zipper)
-
-    # multiple newlines means this isn't a group. missing newline means EOF.
-    # thus only when there's one newline do we continue accumulating for our alias group
-    if meta[:end_of_expression][:newlines] == 1 do
-      consume_alias_group(zipper, aliases)
-    else
-      {zipper, aliases}
-    end
-  end
-
-  defp consume_alias_group(zipper, aliases) do
-    # aliases in groups are always siblings, so we're using `Zipper.right` to save time from going down subtrees
-    case Zipper.right(zipper) do
-      nil -> {zipper, aliases}
-      zipper -> consume_alias_group(zipper, aliases)
-    end
-  end
-
-  # This is where multi alias expansion happens
-  #
   # alias Foo.{Bar, Baz}
   # =>
   # alias Foo.Bar
   # alias Foo.Baz
-  defp expand_alias({:alias, _, [{{:., _, [{_, _, module}, :{}]}, _, right}]}, aliases) do
-    right
-    |> Enum.map(fn {_, meta, segments} -> {:alias, meta, [{:__aliases__, [], module ++ segments}]} end)
-    |> Enum.concat(aliases)
+  defp expand_alias({:alias, _, [{{:., _, [{_, _, module}, :{}]}, _, right}]}) do
+    Enum.map(right, fn {_, meta, segments} -> {:alias, meta, [{:__aliases__, [], module ++ segments}]} end)
   end
 
-  defp expand_alias({:alias, _, _} = alias, aliases), do: [alias | aliases]
+  defp expand_alias(alias), do: [alias]
+
+  defp consume_alias_group([{:alias, meta, _} = alias | siblings], aliases) do
+    if meta[:end_of_expression][:newlines] == 1 do
+      consume_alias_group(siblings, [alias | aliases])
+    else
+      {siblings, [alias | aliases]}
+    end
+  end
+
+  defp consume_alias_group(siblings, aliases), do: {siblings, aliases}
+
+  defp set_newlines({node, meta, children}, newline) do
+    updated_meta = Keyword.update(meta, :end_of_expression, [newlines: newline], &Keyword.put(&1, :newlines, newline))
+    {node, updated_meta, children}
+  end
 end
