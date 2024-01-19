bdbb10f440651ee60c7a58b31e3e8251474c5d76
Milton Mazzarri <me@milmazz.uno>
2024-01-19 14:10:45 -0600

    apply changes asap

--- lib/style/deprecations.ex
+++ lib/style/deprecations.ex
@@ -34,43 +34,41 @@ defmodule Styler.Style.Deprecations do
   defp style({:|>, m, [lhs, {{:., dm, [{:__aliases__, am, [:Path]}, :safe_relative_to]}, funm, args}]}),
     do: {:|>, m, [lhs, {{:., dm, [{:__aliases__, am, [:Path]}, :safe_relative]}, funm, args}]}
 
-  if Version.match?(System.version(), ">= 1.16.0-dev") do
-    # File.stream!(file, options, line_or_bytes) => File.stream!(file, line_or_bytes, options)
-    defp style({{:., _, [{_, _, [:File]}, :stream!]} = f, fm, [path, {:__block__, _, [modes]} = opts, lob]})
-         when is_list(modes),
-         do: {f, fm, [path, lob, opts]}
+  # File.stream!(file, options, line_or_bytes) => File.stream!(file, line_or_bytes, options)
+  defp style({{:., _, [{_, _, [:File]}, :stream!]} = f, fm, [path, {:__block__, _, [modes]} = opts, lob]})
+       when is_list(modes),
+       do: {f, fm, [path, lob, opts]}
 
-    # For ranges where `start > stop`, you need to explicitly include the step
-    # Enum.slice(enumerable, 1..-2) => Enum.slice(enumerable, 1..-2//1)
-    # String.slice("elixir", 2..-1) => String.slice("elixir", 2..-1//1)
-    defp style({{:., _, [{_, _, [module]}, :slice]} = f, funm, [enumerable, {:.., _, [_, _]} = range]})
-         when module in [:Enum, :String],
-         do: {f, funm, [enumerable, add_step_to_decreasing_range(range)]}
+  # For ranges where `start > stop`, you need to explicitly include the step
+  # Enum.slice(enumerable, 1..-2) => Enum.slice(enumerable, 1..-2//1)
+  # String.slice("elixir", 2..-1) => String.slice("elixir", 2..-1//1)
+  defp style({{:., _, [{_, _, [module]}, :slice]} = f, funm, [enumerable, {:.., _, [_, _]} = range]})
+       when module in [:Enum, :String],
+       do: {f, funm, [enumerable, add_step_to_decreasing_range(range)]}
 
-    # File.stream!(file, options, line_or_bytes) => File.stream!(file, line_or_bytes, options)
-    defp style({:|>, m, [lhs, {{_, _, [{_, _, [:File]}, :stream!]} = f, fm, [{:__block__, _, [modes]} = opts, lob]}]})
-         when is_list(modes),
-         do: {:|>, m, [lhs, {f, fm, [lob, opts]}]}
+  # File.stream!(file, options, line_or_bytes) => File.stream!(file, line_or_bytes, options)
+  defp style({:|>, m, [lhs, {{_, _, [{_, _, [:File]}, :stream!]} = f, fm, [{:__block__, _, [modes]} = opts, lob]}]})
+       when is_list(modes),
+       do: {:|>, m, [lhs, {f, fm, [lob, opts]}]}
 
-    # For ranges where `start > stop`, you need to explicitly include the step
-    # Enum.slice(enumerable, 1..-2) => Enum.slice(enumerable, 1..-2//1)
-    # String.slice("elixir", 2..-1) => String.slice("elixir", 2..-1//1)
-    defp style({:|>, m, [lhs, {{:., _, [{_, _, [mod]}, :slice]} = f, funm, [{:.., _, [_, _]} = range]}]})
-         when mod in [:Enum, :String],
-         do: {:|>, m, [lhs, {f, funm, [add_step_to_decreasing_range(range)]}]}
+  # For ranges where `start > stop`, you need to explicitly include the step
+  # Enum.slice(enumerable, 1..-2) => Enum.slice(enumerable, 1..-2//1)
+  # String.slice("elixir", 2..-1) => String.slice("elixir", 2..-1//1)
+  defp style({:|>, m, [lhs, {{:., _, [{_, _, [mod]}, :slice]} = f, funm, [{:.., _, [_, _]} = range]}]})
+       when mod in [:Enum, :String],
+       do: {:|>, m, [lhs, {f, funm, [add_step_to_decreasing_range(range)]}]}
 
-    # ~R is deprecated in favor of ~r
-    defp style({:sigil_R, m, args}), do: {:sigil_r, m, args}
+  # ~R is deprecated in favor of ~r
+  defp style({:sigil_R, m, args}), do: {:sigil_r, m, args}
 
-    # For a decreasing range, we must use Date.range/3 instead of Date.range/2
-    defp style({{:., _, [{:__aliases__, _, [:Date]}, :range]} = funm, dm, [first, last]} = block) do
-      with {:ok, f} <- extract_date_value(first),
-           {:ok, l} <- extract_date_value(last),
-           :gt <- Date.compare(f, l) do
-        {funm, dm, [first, last, -1]}
-      else
-        _ -> block
-      end
+  # For a decreasing range, we must use Date.range/3 instead of Date.range/2
+  defp style({{:., _, [{:__aliases__, _, [:Date]}, :range]} = funm, dm, [first, last]} = block) do
+    with {:ok, f} <- extract_date_value(first),
+         {:ok, l} <- extract_date_value(last),
+         :gt <- Date.compare(f, l) do
+      {funm, dm, [first, last, -1]}
+    else
+      _ -> block
     end
   end
 
@@ -86,24 +84,22 @@ defmodule Styler.Style.Deprecations do
   defp style(node), do: node
 
   # silences "function is unused warnings" on ex < 1.16
-  if Version.match?(System.version(), ">= 1.16.0-dev") do
-    defp add_step_to_decreasing_range({:.., rm, [first, {_, lm, _} = last]} = range) do
-      with {:ok, start} <- extract_value_from_range(first),
-           {:ok, stop} <- extract_value_from_range(last),
-           true <- start > stop do
-        step = {:__block__, [token: "1", line: lm[:line]], [1]}
-        {:"..//", rm, [first, last, step]}
-      else
-        _ -> range
-      end
+  defp add_step_to_decreasing_range({:.., rm, [first, {_, lm, _} = last]} = range) do
+    with {:ok, start} <- extract_value_from_range(first),
+         {:ok, stop} <- extract_value_from_range(last),
+         true <- start > stop do
+      step = {:__block__, [token: "1", line: lm[:line]], [1]}
+      {:"..//", rm, [first, last, step]}
+    else
+      _ -> range
     end
-
-    # Extracts the positive or negative integer from the given range block
-    defp extract_value_from_range({:__block__, _, [value]}) when is_integer(value), do: {:ok, value}
-    defp extract_value_from_range({:-, _, [{:__block__, _, [value]}]}) when is_integer(value), do: {:ok, -value}
-    defp extract_value_from_range(_), do: :non_int
-
-    defp extract_date_value({:sigil_D, _, [{:<<>>, _, [date]}, []]}), do: Date.from_iso8601(date)
-    defp extract_date_value(_), do: :unknown
   end
+
+  # Extracts the positive or negative integer from the given range block
+  defp extract_value_from_range({:__block__, _, [value]}) when is_integer(value), do: {:ok, value}
+  defp extract_value_from_range({:-, _, [{:__block__, _, [value]}]}) when is_integer(value), do: {:ok, -value}
+  defp extract_value_from_range(_), do: :non_int
+
+  defp extract_date_value({:sigil_D, _, [{:<<>>, _, [date]}, []]}), do: Date.from_iso8601(date)
+  defp extract_date_value(_), do: :unknown
 end
--- test/style/deprecations_test.exs
+++ test/style/deprecations_test.exs
@@ -33,62 +33,58 @@ defmodule Styler.Style.DeprecationsTest do
     )
   end
 
-  describe "1.16 deprecations" do
-    @describetag skip: Version.match?(System.version(), "< 1.16.0-dev")
+  test "File.stream!(path, modes, line_or_bytes) to File.stream!(path, line_or_bytes, modes)" do
+    assert_style(
+      "File.stream!(path, [encoding: :utf8, trim_bom: true], :line)",
+      "File.stream!(path, :line, encoding: :utf8, trim_bom: true)"
+    )
 
-    test "File.stream!(path, modes, line_or_bytes) to File.stream!(path, line_or_bytes, modes)" do
-      assert_style(
-        "File.stream!(path, [encoding: :utf8, trim_bom: true], :line)",
-        "File.stream!(path, :line, encoding: :utf8, trim_bom: true)"
-      )
+    assert_style(
+      "f |> File.stream!([encoding: :utf8, trim_bom: true], :line) |> Enum.take(2)",
+      "f |> File.stream!(:line, encoding: :utf8, trim_bom: true) |> Enum.take(2)"
+    )
+  end
 
-      assert_style(
-        "f |> File.stream!([encoding: :utf8, trim_bom: true], :line) |> Enum.take(2)",
-        "f |> File.stream!(:line, encoding: :utf8, trim_bom: true) |> Enum.take(2)"
-      )
-    end
+  test "~R is deprecated in favor of ~r" do
+    assert_style(~s|Regex.match?(~R/foo/, "foo")|, ~s|Regex.match?(~r/foo/, "foo")|)
+  end
 
-    test "~R is deprecated in favor of ~r" do
-      assert_style(~s|Regex.match?(~R/foo/, "foo")|, ~s|Regex.match?(~r/foo/, "foo")|)
-    end
+  test "replace Date.range/2 with Date.range/3 when first > last" do
+    assert_style("Date.range(~D[2000-01-01], ~D[1999-01-01])", "Date.range(~D[2000-01-01], ~D[1999-01-01], -1)")
+    assert_style("Date.range(~D[1999-01-01], ~D[2000-01-01])")
+    assert_style("Date.range(~D[1999-01-01], ~D[1999-01-01])")
+  end
 
-    test "replace Date.range/2 with Date.range/3 when first > last" do
-      assert_style("Date.range(~D[2000-01-01], ~D[1999-01-01])", "Date.range(~D[2000-01-01], ~D[1999-01-01], -1)")
-      assert_style("Date.range(~D[1999-01-01], ~D[2000-01-01])")
-      assert_style("Date.range(~D[1999-01-01], ~D[1999-01-01])")
-    end
+  test "use :eof instead of :all in IO.read/2 and IO.binread/2" do
+    assert_style("IO.read(:all)", "IO.read(:eof)")
+    assert_style("IO.read(device, :all)", "IO.read(device, :eof)")
+    assert_style("IO.binread(:all)", "IO.binread(:eof)")
+    assert_style("IO.binread(device, :all)", "IO.binread(device, :eof)")
 
-    test "use :eof instead of :all in IO.read/2 and IO.binread/2" do
-      assert_style("IO.read(:all)", "IO.read(:eof)")
-      assert_style("IO.read(device, :all)", "IO.read(device, :eof)")
-      assert_style("IO.binread(:all)", "IO.binread(:eof)")
-      assert_style("IO.binread(device, :all)", "IO.binread(device, :eof)")
+    assert_style(
+      "file |> IO.binread(:all) |> :binary.bin_to_list()",
+      "file |> IO.binread(:eof) |> :binary.bin_to_list()"
+    )
+  end
 
-      assert_style(
-        "file |> IO.binread(:all) |> :binary.bin_to_list()",
-        "file |> IO.binread(:eof) |> :binary.bin_to_list()"
-      )
-    end
+  test "negative steps with [Enum|String].slice/2" do
+    for mod <- ~w(Enum String) do
+      assert_style("#{mod}.slice(x, 1..-2)", "#{mod}.slice(x, 1..-2//1)")
+      assert_style("#{mod}.slice(x, -1..-2)", "#{mod}.slice(x, -1..-2//1)")
+      assert_style("#{mod}.slice(x, 2..1)", "#{mod}.slice(x, 2..1//1)")
+      assert_style("#{mod}.slice(x, 1..3)")
+      assert_style("#{mod}.slice(x, ..)")
 
-    test "negative steps with [Enum|String].slice/2" do
-      for mod <- ~w(Enum String) do
-        assert_style("#{mod}.slice(x, 1..-2)", "#{mod}.slice(x, 1..-2//1)")
-        assert_style("#{mod}.slice(x, -1..-2)", "#{mod}.slice(x, -1..-2//1)")
-        assert_style("#{mod}.slice(x, 2..1)", "#{mod}.slice(x, 2..1//1)")
-        assert_style("#{mod}.slice(x, 1..3)")
-        assert_style("#{mod}.slice(x, ..)")
+      # piped
+      assert_style("foo |> bar() |> #{mod}.slice(1..-2)", "foo |> bar() |> #{mod}.slice(1..-2//1)")
+      assert_style("foo |> bar() |> #{mod}.slice(-1..-2)", "foo |> bar() |> #{mod}.slice(-1..-2//1)")
+      assert_style("foo |> bar() |> #{mod}.slice(2..1)", "foo |> bar() |> #{mod}.slice(2..1//1)")
+      assert_style("foo |> bar() |> #{mod}.slice(1..3)")
 
-        # piped
-        assert_style("foo |> bar() |> #{mod}.slice(1..-2)", "foo |> bar() |> #{mod}.slice(1..-2//1)")
-        assert_style("foo |> bar() |> #{mod}.slice(-1..-2)", "foo |> bar() |> #{mod}.slice(-1..-2//1)")
-        assert_style("foo |> bar() |> #{mod}.slice(2..1)", "foo |> bar() |> #{mod}.slice(2..1//1)")
-        assert_style("foo |> bar() |> #{mod}.slice(1..3)")
-
-        # non-trivial ranges
-        assert_style "#{mod}.slice(x, y..z)"
-        assert_style "#{mod}.slice(x, (y - 1)..f)"
-        assert_style("foo |> bar() |> #{mod}.slice(x..y)")
-      end
+      # non-trivial ranges
+      assert_style "#{mod}.slice(x, y..z)"
+      assert_style "#{mod}.slice(x, (y - 1)..f)"
+      assert_style("foo |> bar() |> #{mod}.slice(x..y)")
     end
   end
 end
