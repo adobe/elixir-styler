242146e260b1b8eb822eec212b546f70ff120d12
Matt Enlow <matt@novaugust.net>
2024-01-18 17:50:38 -0700

    keep module callback ordering relative to `use` directives. closes #120

--- lib/style/module_directives.ex
+++ lib/style/module_directives.ex
@@ -69,7 +69,8 @@ defmodule Styler.Style.ModuleDirectives do
   alias Styler.Zipper
 
   @directives ~w(alias import require use)a
-  @attr_directives ~w(moduledoc shortdoc behaviour)a
+  @callback_directives ~w(before_compile after_compile after_verify)a
+  @attr_directives ~w(moduledoc shortdoc behaviour)a ++ @callback_directives
 
   @moduledoc_false {:@, [line: nil], [{:moduledoc, [line: nil], [{:__block__, [line: nil], [false]}]}]}
 
@@ -142,6 +143,9 @@ defmodule Styler.Style.ModuleDirectives do
 
     directives =
       Enum.group_by(directives, fn
+        # callbacks are essentially the same as `use` -- they invoke macros.
+        # so, we need to group / order them with `use` statements to make sure we don't break things!
+        {:@, _, [{callback, _, _}]} when callback in @callback_directives -> :use
         {:@, _, [{attr_name, _, _}]} -> :"@#{attr_name}"
         {directive, _, _} -> directive
       end)
