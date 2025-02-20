aaedd0c9e8c11c55b0ee83e31a2eaab97ae94923
Matt Enlow <enlow@adobe.com>
2025-02-20 13:32:46 -0700

    v1.4.0

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Styler.MixProject do
   use Mix.Project
 
   # Don't forget to bump the README when doing non-patch version changes
-  @version "1.3.3"
+  @version "1.4.0"
   @url "https://github.com/adobe/elixir-styler"
 
   def project do
