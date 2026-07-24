bfa219787d705e51ebf8f952d6b671c2c32ec808
Matt Enlow <enlow@adobe.com>
2026-07-24 13:04:18 -0700

    nix duplicative tests

--- test/style/blocks_test.exs
+++ test/style/blocks_test.exs
@@ -313,30 +313,6 @@ defmodule Styler.Style.BlocksTest do
       )
     end
 
-    test "block swapping comments - comments modify the bodies" do
-      assert_style(
-        """
-        case foo do
-          false ->
-            # a
-            :error
-          true ->
-            # b
-            :ok
-        end
-        """,
-        """
-        if foo do
-          # b
-          :ok
-        else
-          # a
-          :error
-        end
-        """
-      )
-    end
-
     test "complex comments" do
       assert_style(
         """
@@ -1283,58 +1259,6 @@ defmodule Styler.Style.BlocksTest do
         )
       end
     end
-
-    test "comments and flips" do
-      assert_style(
-        """
-        if !a do
-          # b
-          b
-        else
-          # c
-          c
-        end
-        """,
-        """
-        if a do
-          # c
-          c
-        else
-          # b
-          b
-        end
-        """
-      )
-    end
-  end
-
-  describe "comment movement regressions" do
-    test "comments survive swap when do body is multiple lines (negated if)" do
-      assert_style(
-        """
-        if !a do
-          # b1
-          b1
-          # b2
-          b2
-        else
-          # c
-          c
-        end
-        """,
-        """
-        if a do
-          # c
-          c
-        else
-          # b1
-          b1
-          # b2
-          b2
-        end
-        """
-      )
-    end
   end
 
   describe "block swap comment coverage" do
@@ -1418,68 +1342,6 @@ defmodule Styler.Style.BlocksTest do
           x2
           # x3
           x3
-        end
-        """
-      )
-    end
-
-    test "1-statement do body swaps above a 3-statement else" do
-      assert_style(
-        """
-        if !a do
-          # p
-          p
-        else
-          # q1
-          q1
-          # q2
-          q2
-          # q3
-          q3
-        end
-        """,
-        """
-        if a do
-          # q1
-          q1
-          # q2
-          q2
-          # q3
-          q3
-        else
-          # p
-          p
-        end
-        """
-      )
-    end
-
-    test "equal-size (2 statement) do/else bodies swap" do
-      assert_style(
-        """
-        if !a do
-          # m1
-          m1
-          # m2
-          m2
-        else
-          # n1
-          n1
-          # n2
-          n2
-        end
-        """,
-        """
-        if a do
-          # n1
-          n1
-          # n2
-          n2
-        else
-          # m1
-          m1
-          # m2
-          m2
         end
         """
       )
@@ -1623,38 +1485,6 @@ defmodule Styler.Style.BlocksTest do
       )
     end
 
-    test "case to if with equal-size (2 statement) do/else bodies (false first)" do
-      assert_style(
-        """
-        case foo do
-          false ->
-            # d1
-            d1
-            # d2
-            d2
-          true ->
-            # e1
-            e1
-            # e2
-            e2
-        end
-        """,
-        """
-        if foo do
-          # e1
-          e1
-          # e2
-          e2
-        else
-          # d1
-          d1
-          # d2
-          d2
-        end
-        """
-      )
-    end
-
     test "cond to if with multi-line do body and 1-statement else" do
       assert_style(
         """
@@ -1682,9 +1512,7 @@ defmodule Styler.Style.BlocksTest do
         """
       )
     end
-  end
 
-  describe "block swap comment coverage - suspected gaps" do
     test "blank line breaks natural adjacency to the next clause" do
       assert_style(
         """
@@ -1897,38 +1725,6 @@ defmodule Styler.Style.BlocksTest do
           # bar
           e
           # dangling
-        end
-        """
-      )
-    end
-
-    test "another" do
-      assert_style(
-        """
-        case foo do
-          # a
-          false ->
-            # foo
-            d1
-          # b
-          true ->
-            # bar
-            e
-            f
-            # dangling
-        end
-        """,
-        """
-        if foo do
-          # b
-          # bar
-          e
-          f
-          # dangling
-        else
-          # a
-          # foo
-          d1
         end
         """
       )
